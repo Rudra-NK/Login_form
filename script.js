33c54cbc50c517c355f531ada2f3a439b14cbfcc
Rudra ‎ <rudranarayankhatai@gmail.com>
2025-12-22 22:08:43 +0530

    eye button added

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ labels.forEach(label => {
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
 })
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    this.classList.toggle('fa-eye-slash');
+});
+
